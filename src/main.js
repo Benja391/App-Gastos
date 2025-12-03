45d058009449a0fc2a71673e1db069a82516952f
Esporas123456789 <benjabosch5@gmail.com>
2025-12-03 17:08:15 -0300

    configuracion para hacer PWA

--- src/main.js
+++ src/main.js
@@ -8,3 +8,12 @@ const app = createApp(App);
 app.use(router);
 
 app.mount('#app');
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then(() => console.log("Service Worker registrado"))
+      .catch((err) => console.error("Error registrando SW:", err));
+  });
+}
